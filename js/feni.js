bb4c93ead369dab6911ea79f20ed47f3f04c27c8
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2024-09-23 21:38:11 +0600

    Finished feni part

---
+++ js/feni.js
@@ -0,0 +1,36 @@
+document.getElementById('feni-donation-btn')
+ .addEventListener('click', function (event) {
+  event.preventDefault();
+  const donation = getInputFieldById('feni-input');
+  const balance = getTextFieldById('main-balance');
+  const donatedBalance = getTextFieldById('donated-balance-text');
+
+  if (!isNaN(donation) && donation <= balance && donation > 0) {
+   const remainingBalance = balance - donation;
+   document.getElementById('main-balance').innerText = remainingBalance + ' BDT';
+
+   const updatedBalance = donatedBalance + donation;
+   document.getElementById('feni-donated-balance-text').innerText = updatedBalance + ' BDT';
+
+   showSuccessModal();
+
+  } else {
+   alert('Invalid amount! Please enter a valid amount.');
+  }
+
+ })
+
+
+const closeModalButton = document.getElementById('close-modal-btn');
+
+closeModalButton.addEventListener('click', function () {
+ const donationModal = document.getElementById('donation-modal');
+ donationModal.classList.add('hidden');
+});
+
+window.addEventListener('click', function (event) {
+ const donationModal = document.getElementById('donation-modal');
+ if (event.target === donationModal) {
+  donationModal.classList.add('hidden');
+ }
+});
